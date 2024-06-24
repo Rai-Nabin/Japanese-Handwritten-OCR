dc499ea6b2f6c32cfd14e93afdc07eb17222e934
Nabin Rai <kulyangrai10@gmail.com>
2024-06-24 17:20:54 +0545

    Detect Text from Image

--- src/detect_text.py
+++ src/detect_text.py
@@ -17,6 +17,21 @@ class TextDetection:
         """
         bounding_boxes = self.ocr.ocr(image_path, rec=False, cls=False)
         return bounding_boxes
+    
+    @staticmethod
+    def sort_bounding_boxes(bounding_boxes):
+        """Takes first point from each bounding boxes. Then it sorts y-coordinates and if two points have same y-cordinates it looks for x-coordinates.
+
+        Args:
+            bounding_boxes (list): Bounding boxes coordinates of text.
+
+        Returns:
+            list: [[[544.0, 261.0], [1935.0, 226.0], [1938.0, 352.0], [548.0, 387.0]], ...]
+        """
+        sorted_bounding_boxes = sorted(
+            bounding_boxes, key=lambda k: (k[:][0][1], k[:][0][0]))
+
+        return sorted_bounding_boxes
 
     def draw_bounding_box_and_save(self, image, bounding_boxes, output_image_path):
         """Draws bounding boxes around text image and saves it.
