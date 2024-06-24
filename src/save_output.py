64b7e54ca0e8717ba79089f20b3cd11127c7433a
Nabin Rai <kulyangrai10@gmail.com>
2024-06-24 17:22:40 +0545

    Save Extracted Text as JSON

--- src/save_output.py
+++ src/save_output.py
@@ -1,7 +1,7 @@
 import json
 
-def save_to_json(data):
-    with open('output/output.json', 'wb') as fp:
+def save_to_json(data, save_path):
+    with open(save_path, 'wb') as fp:
         fp.write(json.dumps(data, ensure_ascii=False).encode("utf-8"))
     print("Output text saved to JSON.")
 
